8ae321b0be67ac514314099d0954ed080edebc3c
Matthias Lee <matthias.lee@appian.com>
2018-07-16 07:38:18 -0400

    Updated Deep Moor data to point to github repo since the original link no longer works

--- cc/01_parse_weather-data/15_get/main.go
+++ cc/01_parse_weather-data/15_get/main.go
@@ -10,7 +10,9 @@ import (
 )
 
 func main() {
-	res, err := http.Get("http://lpo.dt.navy.mil/data/DM/Environmental_Data_Deep_Moor_2015.txt")
+	// This url used to be at: http://lpo.dt.navy.mil/data/DM/Environmental_Data_Deep_Moor_2015.txt
+	// but has since been discontinued, the below url points to the source repository
+	res, err := http.Get("https://raw.githubusercontent.com/GoesToEleven/lynda/master/cc/01_parse_weather-data/Environmental_Data_Deep_Moor_2015.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
